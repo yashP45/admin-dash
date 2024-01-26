39398ca55e148cbf70d5b235718cb090ad74de15
yash <yashporwal208@gmail.com>
2024-01-26 10:42:25 +0530

    first commit

---
+++ app/dashboard/products/[id]/page.jsx
@@ -0,0 +1,50 @@
+// import { updateUser } from "@/app/lib/actions";
+import { fetchUser } from "@/app/lib/data";
+import styles from "@/app/ui/dashboard/users/singleUser/singleUser.module.css";
+import Image from "next/image";
+
+const SingleUserPage = async ({ params }) => {
+  const { id } = params;
+  const user = await fetchUser(id);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src="/noavatar.png" alt="" fill />
+        </div>
+        {product.title}
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type="hidden" name="id" />
+          <label>Title</label>
+          <input type="text" name="title" />
+          <label>Price</label>
+          <input type="number" name="price" />
+          <label>Stock</label>
+          <input type="number" name="stock" />
+          <label>Color</label>
+          <input type="text" name="color" placeholder={"color"} />
+          <label>Size</label>
+          <textarea type="text" name="size" placeholder={"size"} />
+          <label>Cat</label>
+          <select name="cat" id="cat">
+            <option value="kitchen">Kitchen</option>
+            <option value="computers">Computers</option>
+          </select>
+          <label>Description</label>
+          <textarea
+            name="desc"
+            id="desc"
+            rows="10"
+            // placeholder={product.desc}
+          ></textarea>
+          <button>Update</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUserPage;
